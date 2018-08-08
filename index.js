a1c68ebfdbd0ddd97c890e5effe0c58ed0b9ffdb
Kurbonov Sukhrobjon <abubakr0606@gmail.com>
2018-08-08 11:07:32 +0500

    make it better

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var mysql=require("mysql")
 var link_to_chanel=`@oziquz`;
 var limit=3;
 var channel_id='-1001231331656';
-var Originalchannel_id='@oziquz';
+var Originalchannel_id='@optimus1234q';
 var Admin_id=511599;
 /////////////////////////////////////////////////////
 //var category;
